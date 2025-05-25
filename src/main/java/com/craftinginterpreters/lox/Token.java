2a11e95a7ce61887a89881e332690bda38db2ef1
Rounak Kumar Singh <rounak.official.2@gmail.com>
2025-05-25 17:06:19 +0530

    Parser initial

---
+++ src/main/java/com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,8 @@
+package com.craftinginterpreters.lox;
+
+public record Token(TokenType type, String lexeme, Object literal, int line) {
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
---
+++ src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,23 @@
+package com.craftinginterpreters.lox;
+
+public enum TokenType {
+    // Single Characters
+
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, SEMICOLON, DOT, MINUS, PLUS, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
